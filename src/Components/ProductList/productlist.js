09ee6dda69933775da0a3c1de472909a047d63cb
MohamedAmal <ironhide243@gmail.com>
2022-03-31 21:26:45 +0200

    none

--- src/Components/ProductList/productlist.js
+++ src/Components/ProductList/productlist.js
@@ -11,7 +11,7 @@ class ProductList extends React.Component {
   componentDidMount() {
     console.log('commstate', this.props.commState)
     // const url = 'http://localhost/index.php/'
-    const url = 'https://productsproject.000webhostapp.com/public_html/index.html/'
+    const url = 'https://productsproject.000webhostapp.com/index.php/'
 
     axios.get(url).then(response => response.data).then(data => {
       const dvd = data.filter(obj => obj.Type == 'DVD');
@@ -26,7 +26,7 @@ class ProductList extends React.Component {
   }
   componentDidUpdate() {
     // const url = 'http://localhost/index.php/'
-    const url = 'https://productsproject.000webhostapp.com/public_html/index.html/'
+    const url = 'https://productsproject.000webhostapp.com/index.php/'
       axios.get(url).then(response => response.data).then(data => {
         const dvd = data.filter(obj => obj.Type == 'DVD');
         const furniture = data.filter(obj => obj.Type == 'Furniture');
@@ -44,7 +44,7 @@ class ProductList extends React.Component {
     axios({
       method: 'DELETE',
       // url: 'http://localhost/index.php/?delete=' + this.state.massDelete.join(),
-      url: 'https://productsproject.000webhostapp.com/public_html/index.html/?delete=' + this.state.massDelete.join(),
+      url: 'https://productsproject.000webhostapp.com/index.php/?delete=' + this.state.massDelete.join(),
 
       config: { headers: { 'Content-Type': 'application/json' } }
     })

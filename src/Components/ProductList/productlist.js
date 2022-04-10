10eba3e942b59e9ed37078f510c06409bb53be09
mohamedv123 <ironhide243@gmail.com>
2022-04-10 14:58:04 +0200

    remote success

--- src/Components/ProductList/productlist.js
+++ src/Components/ProductList/productlist.js
@@ -68,9 +68,9 @@ class ProductList extends React.Component {
         url: 'http://productsproject.atwebpages.com/index.php/?delete=' + this.state.massDelete.join(), // remote awardspace
         config: { headers: { 'Content-Type': 'application/json' } }
       }).then(function (response) { console.log(response) }).catch(function (response) { console.log(response) });
-      this.setState({ massDelete: [] })
-      this.setState({ products: [] })
-      this.setState({ deleteAction: true })
+      // this.setState({ massDelete: [] })
+      // this.setState({ products: [] })
+      this.setState({products: [], massDelete: [], deleteAction: true  })
 
     }
   }

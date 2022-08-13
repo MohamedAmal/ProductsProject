e9c2aa54b02c924390eff6d0491e1b6806055c13
MohamedAmal <37916085+MohamedAmal@users.noreply.github.com>
2022-08-13 17:22:17 +0200

    Update productadd.js

--- src/Components/ProductAdd/productadd.js
+++ src/Components/ProductAdd/productadd.js
@@ -249,7 +249,7 @@ class ProductAdd extends React.Component {
             <footer className='container d-flex justify-content-center text-center p-0' >
               <div className='container d-flex flex-column justify-content-center p-4 pt-0'  >
                 <hr className='p-0 m-3 mt-2' style={{ border: '1px solid black' }}></hr>
-                Scandiweb Test assignment
+                ®
               </div>
             </footer>
           </div>
